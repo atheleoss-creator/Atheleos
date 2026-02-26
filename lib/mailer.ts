aad410dc5837004c22a24a43d6c15b592fb48b80
Rohan Kumar Singh <ttrjj7659@gmail.com>
2026-02-26 21:44:21 +0530

    mail

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,9 +1,9 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST || 'smtp.gmail.com',
+    host: process.env.SMTP_HOST || 'smtp.hostinger.com',
     port: parseInt(process.env.SMTP_PORT || '465'),
-    secure: true, // true for 465, false for 587
+    secure: true, // true for 465
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
