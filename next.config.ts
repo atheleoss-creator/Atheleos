e8a92ac6308c8740daf7df8880aa260111929fae
Rohan Kumar Singh <ttrjj7659@gmail.com>
2026-03-09 15:05:44 +0530

    cv

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
   images: {
     unoptimized: true,
   },
-  output: "standalone",
 };
 
 export default nextConfig;
